66d41aa4ced88a0bae60ed0eaef1b692570d5cd6
Ben Croker <ben@putyourlightson.com>
2024-02-25 18:44:17 -0600

    Add test

--- tests/pest/Feature/HintsTest.php
+++ tests/pest/Feature/HintsTest.php
@@ -4,7 +4,6 @@
  * Tests hints functionality.
  */
 
-use putyourlightson\blitzhints\BlitzHints;
 use putyourlightson\blitzhints\records\HintRecord;
 
 beforeEach(function() {
@@ -14,16 +13,25 @@ beforeEach(function() {
 test('Hint is recorded for a related element query that is lazy-loaded', function() {
     saveHint();
 
-    BlitzHints::getInstance()->hints->save();
-
     expect(HintRecord::find()->count())
         ->toEqual(1);
 });
 
+test('Hint is recorded for a related element query that is lazy-loaded with the correct field ID', function() {
+    saveHint();
+
+    /** @var HintRecord $hint */
+    $hint = HintRecord::find()->one();
+    $field = Craft::$app->getFields()->getFieldByHandle('relatedTo');
+
+    expect($hint)
+        ->not()->toBeNull()
+        ->and($hint->fieldId)
+        ->toEqual($field->id);
+});
+
 test('Hint is not recorded for a related element query that is lazy-loaded in a template that exist in the vendor folder path', function() {
     saveHint(Craft::getAlias('@vendor/templates/test'));
-
-    BlitzHints::getInstance()->hints->save();
 
     expect(HintRecord::find()->count())
         ->toEqual(0);
